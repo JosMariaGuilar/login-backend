987f63ebba4b7d57d1160ff7e0fe0fafe5068db4
Jose Maria <josmariaguilar@gmail.com>
2023-02-09 10:06:43 -0400

    added prisma and configure

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,4 +3,11 @@ import { Injectable } from "@nestjs/common";
 @Injectable({})
 export class AuthService {
 
+  signup() {
+    return { msg: 'signup from service' }
+  }
+
+  login() {
+    return { msg: 'signin from service' }
+  }
 }
