ff5b061b4917bf6da71253b97291ee5cb98be48d
Jose Maria <josmariaguilar@gmail.com>
2023-02-14 11:00:09 -0400

    Created decorator to refactor

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
 
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User, @GetUser('username') username: string) {
+    console.log({username});
+    return user;
   }
 }
+ 
